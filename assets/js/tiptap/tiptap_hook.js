5f7900da42a118bd84284fe2feb189739a483fb1
Roig <groig@protonmail.com>
2022-04-16 21:52:34 -0400

    collaboration baby!

--- assets/js/tiptap/tiptap_hook.js
+++ assets/js/tiptap/tiptap_hook.js
@@ -1,39 +1,26 @@
-import * as Y from "yjs";
-import { Observable } from "lib0/observable";
-
-class Provider extends Observable {
-  constructor(ydoc) {
-    super();
-    this.doc = ydoc;
-    ydoc.on("update", (update, origin) => {
-      // ignore updates applied by this provider
-      console.log("ydoc update");
-      console.log(update, origin);
-      if (origin !== this) {
-        // this update was produced either locally or by another provider.
-                     this.emit('update', [update])
-      }
-    });
-    // listen to an event that fires when a remote update is received
-    this.on("update", (update) => {
-      console.log("provider update");
-      console.log("update");
-      Y.applyUpdate(ydoc, update, this); // the third parameter sets the transaction-origin
-    });
-  }
-}
-
 let TiptapHook = {
   async mounted() {
     let core = await import("@tiptap/core");
     let starterKit = await import("@tiptap/starter-kit");
     let collaboration = await import("@tiptap/extension-collaboration");
-    let hocuspocusProvider = await import("@hocuspocus/provider");
+    let y = await import("yjs");
 
     // Set up the Hocuspocus WebSocket provider
-      const ydoc = new Y.Doc()
-    const provider = new Provider(ydoc);
+    const ydoc = new y.Doc();
 
+    ydoc.on("update", (update, origin) => {
+      let base64 = btoa(String.fromCharCode.apply(null, update));
+      this.pushEvent("editor_changed", { update: base64 });
+    });
+
+    this.handleEvent("editor_changed", (data) => {
+      let update = new Uint8Array(
+        atob(data.update)
+          .split("")
+          .map((char) => char.charCodeAt(0))
+      );
+      y.applyUpdate(ydoc, update, this);
+    });
     const editor = new core.Editor({
       element: document.querySelector("#editor"),
       extensions: [
@@ -43,20 +30,7 @@ let TiptapHook = {
         }),
         // Register the document with Tiptap
         collaboration.Collaboration.configure({
-          document: provider.doc,
-        }),
-      ],
-    });
-    const editor1 = new core.Editor({
-      element: document.querySelector("#editor1"),
-      extensions: [
-        starterKit.default.configure({
-          // The Collaboration extension comes with its own history handling
-          history: false,
-        }),
-        // Register the document with Tiptap
-        collaboration.Collaboration.configure({
-          document: provider.doc,
+          document: ydoc,
         }),
       ],
     });
--- lib/today_web/live/tiptap_live.ex
+++ lib/today_web/live/tiptap_live.ex
@@ -1,12 +1,37 @@
 defmodule TodayWeb.TiptapLive do
   use TodayWeb, :live_view
+  alias Phoenix.PubSub
+  alias Today.Utils
 
+  @impl true
+  def mount(_params, _session, socket) do
+    if connected?(socket), do: PubSub.subscribe(Today.PubSub, "tiptap")
+    {:ok, assign(socket, :username, Utils.generate_name())}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
+    <%= @username %>
     <div phx-hook="TiptapHook" id="editors">
       <div id="editor"></div>
-      <div id="editor1"></div>
     </div>
     """
+  end
+
+  @impl true
+  def handle_event("editor_changed", %{} = params, socket) do
+    PubSub.broadcast(Today.PubSub, "tiptap", {:editor_changed, params, socket.assigns.username})
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:editor_changed, data, origin}, socket) do
+    username = socket.assigns.username
+
+    case origin do
+      ^username -> {:noreply, socket}
+      _ -> {:noreply, push_event(socket, "editor_changed", data)}
+    end
   end
 end
