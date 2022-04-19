c3dfdf8eab6be151e36e0b3b0fc327558cdf06c7
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2022-04-19 19:43:10 +0300

    fix: run a single instance of Postgres, indent

--- shell.nix
+++ shell.nix
@@ -23,47 +23,47 @@ let
 
   # define shell startup command
   hooks = ''
-    # this allows mix to work on the local directory
-    echo 'Setting elixir'
-    export MIX_HOME=$PWD/.nix-mix
-    export HEX_HOME=$PWD/.nix-hex
-    export PATH=$MIX_HOME/bin:$PATH
-    export PATH=$HEX_HOME/bin:$PATH
-    export LANG=en_US.UTF-8
-    export ERL_AFLAGS="-kernel shell_history enabled"
-    if [ ! -d $MIX_HOME ]; then
-        echo "Install hex and rebar..."
-        mix local.hex --force
-        mix local.rebar --force
-    fi
-    if [ ! -d $HEX_HOME ]; then
-        echo "Getting dependencies"
-        mix deps.get
-    fi
-    echo 'Setting database'
-    export PGDATA=$PWD/postgres_data
-    export PGHOST=$PWD/postgres
-    export LOG_PATH=$PWD/postgres/LOG
-    export PGDATABASE=postgres
-    export PGSOCKET=$PWD/sockets
-    mkdir $PGSOCKET
-    if [ ! -d $PGHOST ]; then
-      mkdir -p $PGHOST
-    fi
-    if [ ! -d $PGDATA ]; then
-      echo 'Initializing postgresql database...'
-      initdb $PGDATA --auth=trust >/dev/null
-    fi
+  # this allows mix to work on the local directory
+  echo 'Setting elixir'
+  export MIX_HOME=$PWD/.nix-mix
+  export HEX_HOME=$PWD/.nix-hex
+  export PATH=$MIX_HOME/bin:$PATH
+  export PATH=$HEX_HOME/bin:$PATH
+  export LANG=en_US.UTF-8
+  export ERL_AFLAGS="-kernel shell_history enabled"
+  if [ ! -d $MIX_HOME ]; then
+    echo "Install hex and rebar..."
+    mix local.hex --force
+    mix local.rebar --force
+  fi
+  if [ ! -d $HEX_HOME ]; then
+    echo "Getting dependencies"
+    mix deps.get
+  fi
+  echo 'Setting database'
+  export PGDATA=$PWD/postgres_data
+  export PGHOST=$PWD/postgres
+  export LOG_PATH=$PWD/postgres/LOG
+  export PGDATABASE=postgres
+  export PGSOCKET=$PWD/sockets
+  mkdir $PGSOCKET
+  if [ ! -d $PGHOST ]; then
+    mkdir -p $PGHOST
+  fi
+  if [ ! -d $PGDATA ]; then
+    echo 'Initializing postgresql database...'
+    initdb $PGDATA --auth=trust >/dev/null
     pg_ctl start -l $LOG_PATH -o "-c listen_addresses= -c unix_socket_directories=$PGSOCKET"
     createuser -d -h $PGSOCKET postgres
     psql -h $PGSOCKET -c "ALTER USER postgres PASSWORD 'postgres';"
-    function end {
-          echo "Shutting down the database..."
-          pg_ctl stop
-          echo "Removing directories..."
-          rm -rf $PGDATA $PGHOST $PGSOCKET
-        }
-        trap end EXIT
+  fi
+  function end {
+    echo "Shutting down the database..."
+    pg_ctl stop
+    echo "Removing directories..."
+    rm -rf $PGDATA $PGHOST $PGSOCKET
+  }
+  trap end EXIT
   '';
 
 in pkgs.mkShell {
